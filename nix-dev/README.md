da6fc3f28bad21fe123c9891bfce3638912ea430
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-12-07 00:37:12 +0300

    add dev flake

---
+++ nix-dev/README.md
@@ -0,0 +1,20 @@
+# VSCodium generic
+
+## Prerequisites
+
+See [Prerequisites](https://github.com/deemp/flakes#prerequisites)
+
+## Description
+
+This flake is based on the [codium](https://github.com/deemp/flakes/blob/main/codium#readme) flake.
+
+This flake demonstrates how to enable specific extensions and make the `hello` executable available on `PATH` in `VSCodium`.
+
+## Devshell
+
+```console
+nix develop
+codium .
+```
+
+In VSCodium's terminal, run `hello`
---
+++ nix-dev/flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    my-codium.url = "github:deemp/flakes?dir=codium";
+    flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
+    flake-utils.follows = "flake-utils_/flake-utils";
+    vscode-extensions_.url = "github:deemp/flakes?dir=source-flake/vscode-extensions";
+    vscode-extensions.follows = "vscode-extensions_/vscode-extensions";
+    my-devshell.url = "github:deemp/flakes?dir=devshell";
+  };
+  outputs =
+    { self
+    , my-codium
+    , flake-utils
+    , vscode-extensions
+    , my-devshell
+    , ...
+    }: flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        inherit (my-codium.functions.${system}) mkCodium writeSettingsJSON;
+        inherit (my-codium.configs.${system}) extensions settingsNix;
+        devshell = my-devshell.devshell.${system};
+        inherit (my-devshell.functions.${system}) mkCommands;
+        codium = mkCodium {
+          extensions = {
+            inherit (extensions) nix misc markdown github;
+          };
+        };
+        writeSettings = writeSettingsJSON {inherit (settingsNix) nix-ide markdown-all-in-one git gitlens todo-tree;};
+        tools = [codium writeSettings];
+      in
+      {
+        devShells.default = devshell.mkShell
+          {
+            packages = tools;
+            bash = {
+              extra = ''
+                printf "Hello!\n"
+              '';
+            };
+            commands = mkCommands "ide" tools;
+          };
+      });
+
+  nixConfig = {
+    extra-trusted-substituters = [
+      "https://haskell-language-server.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://hydra.iohk.io"
+      "https://deemp.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
+    ];
+  };
+}
