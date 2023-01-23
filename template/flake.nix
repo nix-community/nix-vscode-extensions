6d3ee45f5b87bf1d057da40fac0aa53b467f5f20
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-01-23 20:06:49 +0300

    simplify template

--- template/flake.nix
+++ template/flake.nix
@@ -1,10 +1,8 @@
 {
   inputs = {
-    nixpkgs_.url = "github:deemp/flakes?dir=source-flake/nixpkgs";
-    nixpkgs.follows = "nixpkgs_/nixpkgs";
-    flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
-    flake-utils.follows = "flake-utils_/flake-utils";
-    nix-vscode-extensions.url = "github:deemp/nix-vscode-extensions";
+    nixpkgs.url = "github:NixOS/nixpkgs/dbc68fa4bb132d990945d39801b0d7f2ba15b08f";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
   outputs =
@@ -42,20 +40,5 @@
           '';
         };
       });
-
-  nixConfig = {
-    extra-substituters = [
-      "https://haskell-language-server.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://cache.iog.io"
-      "https://deemp.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
-    ];
-  };
 }
 
