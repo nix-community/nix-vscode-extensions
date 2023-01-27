3ff6f508123be35dc9d01f86cf2141b9c8f6d3bd
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-01-27 11:59:31 +0300

    improve scripts

--- nix-dev/flake.nix
+++ nix-dev/flake.nix
@@ -7,6 +7,7 @@
     vscode-extensions_.url = "github:deemp/flakes?dir=source-flake/vscode-extensions";
     vscode-extensions.follows = "vscode-extensions_/vscode-extensions";
     my-devshell.url = "github:deemp/flakes?dir=devshell";
+    drv-tools.url = "github:deemp/flakes?dir=drv-tools";
   };
   outputs =
     { self
@@ -15,6 +16,7 @@
     , vscode-extensions
     , my-devshell
     , nixpkgs
+    , drv-tools
     , ...
     }: flake-utils.lib.eachDefaultSystem
       (system:
@@ -24,6 +26,9 @@
         inherit (my-codium.configs.${system}) extensions settingsNix;
         devshell = my-devshell.devshell.${system};
         inherit (my-devshell.functions.${system}) mkCommands;
+        inherit (pkgs.lib.attrsets) mapAttrsToList recursiveUpdate mapAttrs;
+        inherit (drv-tools.functions.${system}) mkShellApps concatMapStringsNewline;
+
         codium = mkCodium {
           extensions = {
             inherit (extensions) nix misc markdown github;
@@ -32,7 +37,7 @@
         writeSettings = writeSettingsJSON {
           inherit (settingsNix)
             nix-ide markdown-all-in-one git gitlens todo-tree
-            markdown-language-features
+            markdown-language-features files editor workbench
             ;
         };
         tools = [
@@ -42,96 +47,95 @@
           pkgs.nvfetcher
           pkgs.poetry
         ];
+        env = isTargetOpenVSX: mapAttrs (name: builtins.toString)
+          ({
+            DENO_DIR = "$PWD/.deno";
+            FIRST_BLOCK = 1;
+            THREADS = 0;
+            ACTION_ID = 1;
+            OUT_DIR = "tmp/out";
+          } // (if isTargetOpenVSX then {
+            TARGET = "open-vsx";
+            NAME = "Open VSX";
+            BLOCK_SIZE = 10;
+            BLOCK_LIMIT = 20;
+            APPROX_EXTENSIONS = 3000;
+            ALLOW_NET = "open-vsx.org";
+            MAX_ATTEMPTS = 6;
+            RETRY_WAIT_MINUTES = 61;
+            TIMEOUT_MINUTES = 360;
+          } else {
+            TARGET = "vscode-marketplace";
+            NAME = "VSCode Marketplace";
+            BLOCK_SIZE = 10;
+            BLOCK_LIMIT = 230;
+            APPROX_EXTENSIONS = 45000;
+            ALLOW_NET = "marketplace.visualstudio.com";
+            MAX_ATTEMPTS = 6;
+            RETRY_WAIT_MINUTES = 61;
+            TIMEOUT_MINUTES = 360;
+          }));
         scripts =
           let poetryInstall = "if [[ ! -d .venv ]]; then poetry install; fi"; in
-          {
-            nvfetch = pkgs.writeShellApplication {
-              name = "nvfetch";
-              runtimeInputs = [ pkgs.poetry ];
-              text = ''
-                ${poetryInstall}
-                poetry run python -m scripts.nvfetch \
-                  --target "$TARGET" --first-block "''${FIRST_BLOCK:-1}" \
-                  --block-size "''${BLOCK_SIZE:-1}" --block-limit "''${BLOCK_LIMIT:-1}" \
-                  --threads "''${THREADS:-0}" --action-id "''${ACTION_ID:-1}"
-              '';
-            };
-            combine = pkgs.writeShellApplication {
-              name = "combine";
-              text = ''
-                ${poetryInstall}
-                poetry run python -m scripts.combine \
-                  --target "$TARGET" --out-dir "''${OUT_DIR:-./}"
-              '';
-              runtimeInputs = [ pkgs.poetry ];
-            };
-            generateConfigs = pkgs.writeShellApplication {
-              name = "generate-configs";
-              text = ''     
-                ${poetryInstall}
-                poetry run python -m scripts.generate-configs \
-                  --target "''${TARGET:-vscode-marketplace}" \
-                  --approx-extensions "''${APPROX_EXTENSIONS:-10000}" \
-                  --block-limit "''${BLOCK_LIMIT:-1}" \
-                  --block-size "''${BLOCK_SIZE:-1}" \
-                  --marketplace "''${NAME:-"VSCode Marketplace"}" \
-                  --max-attempts "''${MAX_ATTEMPTS:-3}" \
-                  --retry-wait-minutes "''${RETRY_WAIT_MINUTES:-10}" \
-                  --timeout-minutes "''${TIMEOUT_MINUTES:-10}"
-              '';
-              runtimeInputs = [ pkgs.poetry ];
-            };
-            updateExtensions = pkgs.writeShellApplication {
-              name = "update-extensions";
-              text = ''
-                mkdir -p nvfetch
-                export DENO_DIR=.deno && nix run nixpkgs#deno -- run \
-                  --allow-write --allow-net="$ALLOW_NET" \
-                  --no-prompt updater/index.ts "$TARGET" \
-                  nvfetch/"$TARGET".toml
-              '';
-            };
-          };
+          mkShellApps (
+            mapAttrs (name: value: value // { text = concatMapStringsNewline (x: value.text (env x)) [ true false ]; })
+              {
+                nvfetch = {
+                  runtimeInputs = [ pkgs.poetry ];
+                  text = _: with _; ''
+                    ${poetryInstall}
+                    poetry run python -m scripts.nvfetch \
+                      --target "${TARGET}" \
+                      --first-block "${FIRST_BLOCK}" \
+                      --block-size "${BLOCK_SIZE}" \
+                      --block-limit "${BLOCK_LIMIT}" \
+                      --threads "${THREADS}" \
+                      --action-id "${ACTION_ID}"
+                  '';
+                };
+                combine = {
+                  text = _: with _; ''
+                    ${poetryInstall}
+                    poetry run python -m scripts.combine \
+                      --target "${TARGET}" \
+                      --out-dir "${OUT_DIR}"
+                  '';
+                  runtimeInputs = [ pkgs.poetry ];
+                };
+                generateConfigs = {
+                  text = _: with _; ''     
+                    ${poetryInstall}
+                    poetry run python -m scripts.generate-configs \
+                      --target "${TARGET}" \
+                      --approx-extensions "${APPROX_EXTENSIONS}" \
+                      --block-limit "${BLOCK_LIMIT}" \
+                      --block-size "${BLOCK_SIZE}" \
+                      --marketplace "${NAME}" \
+                      --max-attempts "${MAX_ATTEMPTS}" \
+                      --retry-wait-minutes "${RETRY_WAIT_MINUTES}" \
+                      --timeout-minutes "${TIMEOUT_MINUTES}"
+                  '';
+                  runtimeInputs = [ pkgs.poetry ];
+                };
+                updateExtensions = {
+                  text = _: with _; ''
+                    mkdir -p nvfetch
+                    export DENO_DIR=${DENO_DIR} && nix run nixpkgs#deno -- run \
+                      --allow-write \ 
+                      --allow-net="${ALLOW_NET}" \
+                      --no-prompt \
+                      updater/index.ts "${TARGET}" \
+                      nvfetch/"${TARGET}".toml
+                  '';
+                };
+              });
       in
       {
         packages = scripts;
-        devShells.default = devshell.mkShell
-          {
-            packages = tools;
-            bash = {
-              extra = ''
-                # nvfetch
-                export DENO_DIR="$(pwd)/.deno"
-                export FIRST_BLOCK=1
-                export THREADS=0
-                export ACTION_ID=1
-                export OUT_DIR=tmp/out
-
-                # export TARGET=vscode-marketplace
-                export TARGET=open-vsx
-                if [[ $TARGET = vscode-marketplace ]]; then
-                  export NAME="VSCode Marketplace"
-                  export BLOCK_SIZE=10
-                  export BLOCK_LIMIT=200
-                  export APPROX_EXTENSIONS=45000
-                  export ALLOW_NET=marketplace.visualstudio.com
-                  export MAX_ATTEMPTS=6
-                  export RETRY_WAIT_MINUTES=61
-                  export TIMEOUT_MINUTES=360
-                else
-                  export NAME="Open VSX"
-                  export BLOCK_SIZE=10
-                  export BLOCK_LIMIT=20
-                  export APPROX_EXTENSIONS=3000
-                  export ALLOW_NET=open-vsx.org
-                  export MAX_ATTEMPTS=6
-                  export RETRY_WAIT_MINUTES=61
-                  export TIMEOUT_MINUTES=360
-                fi
-              '';
-            };
-            commands = mkCommands "ide" tools;
-          };
+        devShells.default = devshell.mkShell {
+          packages = tools;
+          commands = mkCommands "ide" tools;
+        };
       });
 
   nixConfig = {
