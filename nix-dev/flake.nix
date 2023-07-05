15b711ebffa7c523bf6ec979f22157794c1f95e5
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-05 20:22:25 +0300

    upd dev flake

--- nix-dev/flake.nix
+++ nix-dev/flake.nix
@@ -18,22 +18,12 @@
           (system:
           let
             pkgs = inputs.nixpkgs.legacyPackages.${system};
-            inherit (inputs.codium.lib.${system}) mkCodium writeSettingsJSON extensions settingsNix;
+            inherit (inputs.codium.lib.${system}) mkCodium writeSettingsJSON extensionsCommon settingsCommonNix;
             inherit (inputs.devshell.lib.${system}) mkRunCommandsDir mkShell;
             inherit (inputs.drv-tools.lib.${system}) mkShellApps;
             packages = {
-              codium = mkCodium {
-                extensions = {
-                  inherit (extensions) nix misc markdown github;
-                };
-              };
-              writeSettings = writeSettingsJSON {
-                inherit (settingsNix)
-                  nix-ide markdown-all-in-one git gitlens todo-tree
-                  markdown-language-features files workbench editor
-                  errorlens
-                  ;
-              };
+              codium = mkCodium { extensions = extensionsCommon; };
+              writeSettings = writeSettingsJSON settingsCommonNix;
             } //
             (mkShellApps {
               updateExtensions = {
@@ -61,13 +51,11 @@
 
   nixConfig = {
     extra-trusted-substituters = [
-      "https://haskell-language-server.cachix.org"
       "https://nix-community.cachix.org"
       "https://hydra.iohk.io"
       "https://deemp.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
