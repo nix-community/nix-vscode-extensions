a6adf7efd7edb948ec95600b801737b64715e09a
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-10 22:19:49 +0300

    move flake

---
+++ haskell/default.nix
@@ -0,0 +1,15 @@
+(import
+  (
+    let
+      lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.flakes.locked;
+    in
+    (
+      import
+        "${fetchTarball {
+          url = "https://github.com/deemp/flakes/archive/${lock.rev}.tar.gz";
+          sha256 = lock.narHash;
+        }}/source-flake"
+    ).outputs.flake-compat.outPath
+  )
+  { src = ./.; }
+).defaultNix
