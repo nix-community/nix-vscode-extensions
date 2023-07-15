522fff3bf25eb5001083b68712c0414f0d0926f3
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-15 18:30:45 +0300

    update template

--- template/flake.nix
+++ template/flake.nix
@@ -1,47 +1,36 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/dbc68fa4bb132d990945d39801b0d7f2ba15b08f";
-    flake-utils.url = "github:numtide/flake-utils";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/13378a0b6026e8b52ccb881454b43201cbe005b4";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/c43d9089df96cf8aca157762ed0e2ddca9fcd71e";
+    flake-utils.follows = "nix-vscode-extensions/flake-utils";
+    nixpkgs.follows = "nix-vscode-extensions/nixpkgs";
   };
 
-  outputs =
-    { self
-    , flake-utils
-    , nixpkgs
-    , nix-vscode-extensions
-    , ...
-    }:
-    flake-utils.lib.eachDefaultSystem
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem
       (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          extensions = inputs.nix-vscode-extensions.extensions.${system};
+          inherit (pkgs) vscode-with-extensions vscodium;
 
-        extensions = nix-vscode-extensions.extensions.${system};
-        codium =
-          let
-            inherit (pkgs) vscode-with-extensions vscodium;
-            someExtensions = builtins.attrValues {
-              inherit (extensions.vscode-marketplace.golang) go;
-              inherit (extensions.vscode-marketplace.vlanguage) vscode-vlang;
+          packages.codium =
+            vscode-with-extensions.override {
+              vscode = vscodium;
+              vscodeExtensions = [
+                extensions.vscode-marketplace.golang.go
+                extensions.open-vsx-release.rust-lang.rust-analyzer
+              ];
             };
-          in
-          (vscode-with-extensions.override {
-            vscode = vscodium;
-            vscodeExtensions = someExtensions;
-          });
-      in
-      {
-        packages = {
-          inherit codium;
-        };
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ codium ];
-          shellHook = ''
-            printf "VSCodium with extensions:\n"
-            codium --list-extensions
-          '';
-        };
-      });
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ packages.codium ];
+            shellHook = ''
+              printf "VSCodium with extensions:\n"
+              codium --list-extensions
+            '';
+          };
+        in
+        {
+          inherit packages devShells;
+        });
 }
 
