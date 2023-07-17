6f57171bc8c798c474cc0c8ff64a4988bef482a8
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-18 00:59:14 +0300

    upd workflow

--- nix-dev/flake.nix
+++ nix-dev/flake.nix
@@ -22,46 +22,68 @@
             pkgs = inputs.nixpkgs.legacyPackages.${system};
             inherit (inputs.codium.lib.${system}) mkCodium writeSettingsJSON extensionsCommon settingsCommonNix;
             inherit (inputs.devshell.lib.${system}) mkRunCommandsDir mkShell;
-            inherit (inputs.drv-tools.lib.${system}) mkShellApps withDescription;
+            inherit (inputs.drv-tools.lib.${system}) mkShellApps withDescription getExe;
             inherit (inputs.workflows.lib.${system}) writeWorkflow nixCI os run;
 
-            packages = {
-              codium = mkCodium { extensions = extensionsCommon; };
-              writeSettings = writeSettingsJSON settingsCommonNix;
-              updateExtensions = withDescription inputs.haskell.packages.${system}.updateExtensions (x: ''Update extensions.'');
-              writeWorkflows = writeWorkflow "ci" (nixCI {
-                strategy.matrix.os = [ os.ubuntu-22 ];
-                cacheNixArgs = {
-                  linuxMaxStoreSize = 5000000000;
-                  macosGCEnabled = false;
-                  keyJob = "update";
-                  files = [ "**/flake.nix" "**/flake.lock" "haskell/**/*" ];
-                };
-                dir = "nix-dev/";
-                doRemoveCacheProfiles = false;
-                doPushToCachix = false;
-                doUpdateLocks = false;
-                doFormat = false;
-                steps = dir: [
-                  (
-                    let name = "Update extensions"; in
+            packages =
+              let scripts =
+                (mkShellApps {
+                  updateExtensions = {
+                    text = getExe inputs.haskell.packages.${system}.updateExtensions;
+                    description = ''Update extensions.'';
+                  };
+                  updateExtraExtensions = {
+                    text = "${getExe pkgs.nvfetcher} -c extra-extensions.toml -o data/extra-extensions";
+                    description = "Update extra extensions.";
+                  };
+                });
+              in
+              scripts //
+              {
+                codium = mkCodium { extensions = extensionsCommon; };
+                writeSettings = writeSettingsJSON settingsCommonNix;
+                writeWorkflows = writeWorkflow "ci" (nixCI {
+                  strategy.matrix.os = [ os.ubuntu-22 ];
+                  cacheNixArgs = {
+                    linuxMaxStoreSize = 5000000000;
+                    macosGCEnabled = false;
+                    keyJob = "update";
+                    files = [ "**/flake.nix" "**/flake.lock" "haskell/**/*" ];
+                  };
+                  dir = "nix-dev/";
+                  doRemoveCacheProfiles = false;
+                  doPushToCachix = false;
+                  doUpdateLocks = false;
+                  doFormat = false;
+                  steps = dir: [
+                    (
+                      let name = "Update extensions"; in
+                      {
+                        inherit name;
+                        env.CONFIG = ".github/config.yaml";
+                        run = run.nixScript { inherit dir; name = scripts.updateExtensions.pname; commitMessage = name; };
+                      }
+                    )
+                    (
+                      (
+                        let name = "Update extra extensions"; in
+                        {
+                          inherit name;
+                          run = run.nixScript { inherit dir; name = scripts.updateExtraExtensions.pname; commitMessage = name; };
+                        }
+                      )
+                    )
                     {
-                      inherit name;
-                      env.CONFIG = ".github/config.yaml";
-                      run = run.nixScript { inherit dir; name = "updateExtensions"; commitMessage = name; };
+                      name = "Commit and push changes.";
+                      run = ''
+                        git add .
+                        git commit --allow-empty -m "action: update extensions"
+                        git push
+                      '';
                     }
-                  )
-                  {
-                    name = "Commit and push changes.";
-                    run = ''
-                      git add .
-                      git commit --allow-empty -m "action: update extensions"
-                      git push
-                    '';
-                  }
-                ];
-              });
-            };
+                  ];
+                });
+              };
 
             tools = [ pkgs.nvfetcher ];
 
