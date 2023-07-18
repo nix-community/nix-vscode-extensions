bddf91998b4cb7a293a2ae688742cf5d544765d4
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-18 18:12:42 +0300

    upd template

--- template/flake.nix
+++ template/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/c43d9089df96cf8aca157762ed0e2ddca9fcd71e";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     flake-utils.follows = "nix-vscode-extensions/flake-utils";
     nixpkgs.follows = "nix-vscode-extensions/nixpkgs";
   };
@@ -13,7 +13,7 @@
           extensions = inputs.nix-vscode-extensions.extensions.${system};
           inherit (pkgs) vscode-with-extensions vscodium;
 
-          packages.codium =
+          packages.default =
             vscode-with-extensions.override {
               vscode = vscodium;
               vscodeExtensions = [
@@ -21,8 +21,9 @@
                 extensions.open-vsx-release.rust-lang.rust-analyzer
               ];
             };
+
           devShells.default = pkgs.mkShell {
-            buildInputs = [ packages.codium ];
+            buildInputs = [ packages.default ];
             shellHook = ''
               printf "VSCodium with extensions:\n"
               codium --list-extensions
