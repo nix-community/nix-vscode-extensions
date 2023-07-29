e8dcee321fc2ca960765db3db5501c3b9b8bff72
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-29 22:13:51 +0300

    nix-dev, haskell: refactor default.nix

--- haskell/default.nix
+++ haskell/default.nix
@@ -1,15 +1,1 @@
-(import
-  (
-    let
-      lock = (builtins.fromJSON (builtins.readFile ../nix-dev/flake.lock)).nodes.flakes.locked;
-    in
-    (
-      import
-        "${fetchTarball {
-          url = "https://github.com/deemp/flakes/archive/${lock.rev}.tar.gz";
-          sha256 = lock.narHash;
-        }}/source-flake"
-    ).outputs.flake-compat.outPath
-  )
-  { src = ./.; }
-).defaultNix
+(builtins.getFlake "github:deemp/flakes/${(builtins.fromJSON (builtins.readFile ../nix-dev/flake.lock)).nodes.flakes.locked.rev}").outputs.makeDefault ./.
