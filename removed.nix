6d1ebf6b9369b1c6a5d759b195d37389bd71b6fe
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-02-27 13:13:30 +0300

    fix(nix): remove ms-dotnettools.csharp

--- removed.nix
+++ removed.nix
@@ -1,4 +1,6 @@
 {
   # https://github.com/nix-community/nix-vscode-extensions/issues/69
   "ms-vscode" = [ "cpptools" ];
+  # https://github.com/nix-community/nix-vscode-extensions/issues/71
+  "ms-dotnettools" = [ "csharp" ];
 }
