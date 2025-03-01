3c059f8d13c6364d86e2c79c1b43aee1e5dfa2f3
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-03-01 14:09:55 +0300

    fix(nix): make ms-vsliveshare.vsliveshare build

--- mkExtension.nix
+++ mkExtension.nix
@@ -41,6 +41,15 @@ let
         chmod +x "$PWD/dist/install scripts/dotnet-install.sh"
       '';
     };
+
+    ms-vsliveshare.vsliveshare = _: {
+      # Similar to https://github.com/NixOS/nixpkgs/blob/6f5808c6534d514751d6de0e20aae83f45d9f798/pkgs/applications/editors/vscode/extensions/ms-vsliveshare.vsliveshare/default.nix#L15-L18
+      # Not sure it's necessary
+      postPatch = ''
+        substituteInPlace vendor.js \
+          --replace-fail '"xsel"' '"${pkgs.xsel}/bin/xsel"'
+      '';
+    };
   };
 
   applyMkExtension = builtins.mapAttrs (
