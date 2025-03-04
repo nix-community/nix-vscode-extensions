b34c18c29ceeff3c676cfb18a1b38a757ac43561
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-03-04 15:36:35 +0300

    refactor(nix): - use flake-parts and devshell - update inputs - remove code for generating workflows

--- nix-dev/flake.nix
+++ nix-dev/flake.nix
@@ -1,168 +1,78 @@
 {
   inputs = {
-    flakes.url = "github:deemp/flakes";
-    nixpkgs.url = "github:nixos/nixpkgs/23ff7d9dc4f3d553939e7bfe0d2667198f993536";
+    nixpkgs.url = "github:nixos/nixpkgs/3a05eebede89661660945da1f151959900903b6a";
+    cache-nix-action = {
+      url = "github:nix-community/cache-nix-action";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.devshell.follows = "devshell";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell = {
+      url = "github:deemp/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.inputs.systems.follows = "systems";
+    };
   };
   outputs =
     inputs:
-    let
-      flakes = inputs.flakes;
-    in
-    flakes.makeFlake {
-      inputs = {
-        inherit (flakes.all)
-          drv-tools
-          devshell
-          codium
-          workflows
-          flakes-tools
-          ;
-        inherit flakes;
-        haskell = import ../haskell;
-        inherit (inputs) nixpkgs;
-      };
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.devshell.flakeModule ];
       perSystem =
-        { inputs, system }:
+        {
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
         let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-          inherit (inputs.codium.lib.${system})
-            mkCodium
-            writeSettingsJSON
-            extensionsCommon
-            settingsCommonNix
-            ;
-          inherit (inputs.devshell.lib.${system}) mkRunCommandsDir mkShell mkCommands;
-          inherit (inputs.drv-tools.lib.${system}) mkShellApps getExe;
-          inherit (inputs.workflows.lib.${system})
-            writeWorkflow
-            nixCI
-            os
-            run
-            expr
-            names
-            steps
-            ;
-          inherit (inputs.flakes-tools.lib.${system}) mkFlakesTools;
+          haskell = import ../haskell;
 
-          packages =
-            let
-              scripts = (
-                mkShellApps {
-                  updateExtensions = {
-                    text = "${getExe inputs.haskell.packages.${system}.default} $@";
-                    description = "Update extensions";
-                  };
-                  updateExtraExtensions = {
-                    text = "${getExe pkgs.nvfetcher} -c extra-extensions.toml -o data/extra-extensions";
-                    description = "Update extra extensions";
-                  };
-                }
-              );
-            in
-            scripts
-            // {
-              codium = mkCodium { extensions = extensionsCommon; };
-              writeSettings = writeSettingsJSON settingsCommonNix;
-              inherit
-                (mkFlakesTools {
-                  dirs = [ "template" ];
-                  root = ../.;
-                })
-                updateLocks
-                ;
-              inherit
-                (mkFlakesTools {
-                  dirs = [
-                    "."
-                    "template"
-                    "nix-dev"
-                    "haskell"
-                  ];
-                  root = ../.;
-                })
-                format
-                saveFlakes
-                ;
+          mkShellApps = lib.mapAttrs (
+            name: value:
+            if !(lib.isDerivation value) && lib.isAttrs value then
+              pkgs.writeShellApplication (value // { inherit name; })
+            else
+              value
+          );
 
-              writeWorkflows = writeWorkflow "ci" (nixCI {
-                jobArgs = {
-                  cacheNixArgs = {
-                    gcEnabledLinux = true;
-                    gcMaxStoreSizeLinux = 7500000000;
-                    purgeEnabled = true;
-                    purgeByCreatedTime = true;
-                    purgeMaxAge = 86400;
-                    debug = true;
-                    keyJob = "update";
-                    files = [
-                      "**/flake.nix"
-                      "**/flake.lock"
-                      "haskell/**/*"
-                    ];
-                    keyOS = expr names.runner.os;
-                  };
-                  dir = "nix-dev/";
-                  doCommit = false;
-                  strategy = { };
-                  runsOn = os.ubuntu-22;
-                  steps =
-                    { dir, stepsAttrs }:
-                    [
-                      {
-                        name = "Update extensions";
-                        env.CONFIG = ".github/config.yaml";
-                        run = run.nixScript {
-                          inherit dir;
-                          name = scripts.updateExtensions.pname;
-                        };
-                      }
-                      {
-                        name = "Update extra extensions";
-                        run = run.nixScript {
-                          inherit dir;
-                          name = scripts.updateExtraExtensions.pname;
-                        };
-                      }
-                      {
-                        name = "Commit and push changes";
-                        run = run.commit {
-                          messages = [
-                            (steps.updateLocks { }).name
-                            stepsAttrs."Update extensions".name
-                            stepsAttrs."Update extra extensions".name
-                          ];
-                        };
-                      }
-                      {
-                        name = "Check template VSCodium";
-                        run = ''
-                          nix profile install template/
-                          nix run template/ -- --list-extensions
-                        '';
-                      }
-                    ];
-                };
-              });
+          packages = mkShellApps {
+            updateExtensions = {
+              text = "${haskell.outputs.packages.${system}.default} /# $@";
+              meta.description = "Update extensions";
             };
 
-          tools = [ pkgs.nvfetcher ];
+            updateExtraExtensions = {
+              text = "${lib.meta.getExe pkgs.nvfetcher} -c extra-extensions.toml -o data/extra-extensions";
+              meta.description = "Update extra extensions";
+            };
+          };
 
-          devShells.default = mkShell {
-            packages = tools;
-            commands =
-              mkRunCommandsDir "nix-dev/" "ide" {
-                "codium ." = packages.codium;
-                inherit (packages) writeSettings;
-              }
-              ++ mkRunCommandsDir "nix-dev/" "scripts" {
-                inherit (packages) updateExtensions updateExtraExtensions;
-              }
-              ++ mkRunCommandsDir "nix-dev/" "infra" { inherit (packages) writeWorkflows updateLocks; }
-              ++ mkCommands "tools" tools;
+          devshells.default = {
+            commands = {
+              tools = [
+                {
+                  expose = true;
+                  packages = {
+                    inherit (pkgs) nvfetcher;
+                  };
+                }
+                {
+                  prefix = "nix run nix-dev/#";
+                  packages = {
+                    inherit (self'.packages) updateExtensions updateExtraExtensions;
+                  };
+                }
+              ];
+            };
           };
         in
         {
-          inherit packages devShells;
+          inherit packages devshells;
         };
     };
 
@@ -170,12 +80,10 @@
     extra-trusted-substituters = [
       "https://nix-community.cachix.org"
       "https://hydra.iohk.io"
-      "https://deemp.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
     ];
   };
 }
