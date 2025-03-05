nix-community/nix-vscode-extensions

commit d99c32428d69c7ce592076cd9d182744c07a0786

refactor(nix): explain problematic extensions in more detail

mkExtension.nix
@@ -86,22 +86,26 @@ let
     callPackage "${pathNixpkgs}/pkgs/applications/editors/vscode/extensions/default.nix"
       { config.allowAliases = false; };
 
-  # For these extensions, some arguments of their function that produces a derivation
-  # are provided in the `let .. in` expression before the call to that function
-  #
-  # TODO override the arguments
-  # Or, make a PR to nixpkgs to simplify overriding derivations
-  haveFixesNonOverridable = [
-    "anweber.vscode-httpyac"
-    "chenglou92.rescript-vscode"
-    # Wait for https://github.com/NixOS/nixpkgs/pull/383013 to be merged
-    "vadimcn.vscode-lldb"
-    "rust-lang.rust-analyzer"
-    # Doesn't build due to the patch
-    # https://github.com/NixOS/nixpkgs/tree/a3cd526f08839bd963e7d106b7869694b0579a94/pkgs/applications/editors/vscode/extensions/hashicorp.terraform
-    # TODO newer fix
-    "hashicorp.terraform"
-  ];
+  extensionsProblematic =
+    # Some arguments of the function that produces a derivation
+    # are provided in the `let .. in` expression before the call to that function
+
+    # TODO make a PR to nixpkgs to simplify overriding for these extensions
+    [
+      "anweber.vscode-httpyac"
+      "chenglou92.rescript-vscode"
+      # Wait for https://github.com/NixOS/nixpkgs/pull/383013 to be merged
+      "vadimcn.vscode-lldb"
+      "rust-lang.rust-analyzer"
+    ]
+    ++
+    # Have old fixes
+    [
+      # Doesn't build due to the patch
+      # https://github.com/NixOS/nixpkgs/tree/a3cd526f08839bd963e7d106b7869694b0579a94/pkgs/applications/editors/vscode/extensions/hashicorp.terraform
+      # TODO newer fix
+      "hashicorp.terraform"
+    ];
 
   pathSpecial = {
     ms-ceintl = "language-packs.nix";

commit 52545f3364419305b00d25e17dbf5116999425bc

fix(nix): fix undefined variable
Fixes https://github.com/nix-community/nix-vscode-extensions/issues/104,
which was introduced in d99c32428d69c7ce592076cd9d182744c07a0786.

mkExtension.nix
@@ -137,7 +137,7 @@ let
               extension;
         in
         { mktplcRef, vsix }@extensionConfig:
-        if builtins.elem extensionId haveFixesNonOverridable then
+        if builtins.elem extensionId extensionsProblematic then
           buildVscodeMarketplaceExtension extensionConfig
         else
           (extension'.override or (abort "${publisher}.${name}")) extensionConfig