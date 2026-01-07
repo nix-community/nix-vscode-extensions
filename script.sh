9105a8f0b15ae40064092b1cbac7cdbf010966ec
Danila Danko <48378098+deemp@users.noreply.github.com>
2026-01-07 18:53:08 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://korol-i-shut.su/albums/ 2> /dev/null | grep -e "<a href=\"https.*</a>" | awk -F '"' '$0=$2' | xargs curl 2> /dev/null | grep -e "<a href=\"https.*songs.*</a>" | awk -F '"' '$0=$2' | xargs curl 2> /dev/null | grep "<article>" | grep -i $1 | awk '{gsub("<br/>", "\n"); gsub("<article>", ""); gsub("</article>", ""); gsub("<p>", "\n"); gsub("</p>", ""); print}'
