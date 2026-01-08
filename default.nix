71eec301014562b1ef78abae41e9ded0cc2a704f
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-01-08 20:10:27 +0300

    fix(nix): don't use a `default.nix` in `nix-dev`

--- default.nix
+++ default.nix
@@ -1,1 +1,9 @@
-(import (import ./nix-dev).inputs.flake-compat { src = ./.; }).defaultNix
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./nix-dev/flake.lock);
+  in
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  }
+) { src = ./.; }).defaultNix
