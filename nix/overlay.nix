ca3f5d4e5640903556616150579ea87b5eb5cc25
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-07-22 13:23:46 +0300

    refactor(nix): move 3 to the right position

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -16,10 +16,10 @@ let
     {
       "0" = platformUniversal;
       "1" = systemPlatform.x86_64-linux;
+      "2" = systemPlatform.aarch64-linux;
       # x86_64-darwin is dropped
       # See https://nixos.org/manual/nixpkgs/unstable/release-notes#x86_64-darwin-26.11
       # "3" = systemPlatform.x86_64-darwin;
-      "2" = systemPlatform.aarch64-linux;
       "4" = systemPlatform.aarch64-darwin;
     }
     .${builtins.toString number} or (builtins.throw "Platform not recognized: ${numberStr}");
